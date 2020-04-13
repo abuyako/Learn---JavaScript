8ddcdf49527b2d8466c340b5414e7d70ed1ff366
abuyako <abuyako@gmail.com>
2020-04-13 15:04:12 +0300

    learning about fetch() sending request and load new info

--- API.js
+++ API.js
@@ -7,3 +7,14 @@
 //     and All of that without reloading the page.
 
 
+// There and Umbrella term AJAX ( Asynchronous Javascript and XML ) for network request from Javascript.
+// There are multiple ways of sending network request and get information from the server.
+
+// The fetch() method is the mordern and versatile, so well start with it. it's not suppodrted by old browsers
+
+// syntax 
+// let promise = fetch(url, [Option])
+
+// url - the url to access 
+// option - optional paremeters: method, Headers etc. 
+
