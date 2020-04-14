67c6ca6200f3c90af3464a3ccf4a33d6ada63cea
abuyako <abuyako@gmail.com>
2020-04-14 11:34:42 +0300

    Learning Promises

---
+++ Promises.js
@@ -0,0 +1,13 @@
+// // PROMISES
+// Promises is the placeholder for something in the future. 
+// So Promises help us in a Asynchronous programming. 
+
+// We have multiple activities happen in out application 
+//  connect();
+//  dbCall();
+//  restCall();
+
+//  Each one of them synchronous at  runtime, if one of them take a lot of time we can excute on of them and other parallel
+//  before the promises we have a callback function myCallBack(); to call the other. 
+ 
+//  better error Handling.
