72592f5b4723ab5e6fbfb6375e28a2d2e7b40109
abuyako <abuyako@gmail.com>
2020-04-22 11:35:28 +0300

    asyn definition and simple example

--- asyn.js
+++ asyn.js
@@ -1,1 +1,12 @@
- 
+// //  ASYNCHRONOUS JAVASCRIPT
+// is the code that keeps running at the backgrouds while our main code is still excuted. 
+// example request some data from a remote server like an API's
+
+// code samples
+
+const first = () => {
+    console.log('Hey there!!');
+}
+
+
+first();
